e3853e07d9ee91ba624f979e7854b83c53f155db
Arpit Yadav <arpityadavay2005@gmail.com>
2024-04-30 18:27:16 +0530

    working on login page - firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
+import { useEffect } from "react";
 import "./App.css";
 import Chat from "./Components/chat/Chat";
 import Details from "./Components/details/Details";
 import List from "./Components/list/List";
 import Login from "./Components/login/Login";
 import Notification from "./Components/notification/Notification";
+import { auth } from "./lib/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import useUserStore from "./lib/userStore";
 
 function App() {
-  const user = false;
+  const {currentUser, isLoading, fetchUserInfo} = useUserStore();
 
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user.uid);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [fetchUserInfo]);
+
+  console.log(currentUser);
+
+  if (isLoading) return <div className="h-[10vh] w-[24vw] bg-white/10 flex justify-center items-center text-xl m-auto rounded-xl"><h1>Loading...</h1></div>
   return (
     <>
       <div className="h-full w-[85vw] flex py-10">
-        {user ? (
+        {currentUser ? (
           <>
             <List />
             <Chat />
--- src/Components/login/Login.jsx
+++ src/Components/login/Login.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import { toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import "react-toastify/dist/ReactToastify.css";
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../../lib/firebase";
 import { db } from "../../lib/firebase";
 import { doc, setDoc } from "firebase/firestore";
@@ -10,57 +13,92 @@ import upload from "../../lib/upload";
 const Login = () => {
     const [avatar, setAvatar] = useState({
         file: null,
-        url: ""
+        url: "",
     });
 
-    const handleAvatar = (e) => {
-        setAvatar ({
-            file:e.target.files[0],
-            url: URL.createObjectURL(e.target.files[0])
-        })
-    }
+    const [loading, setLoading] = useState(false);
 
-    const handleLogin = (e) => {
-        e.preventDefault()
-    }
+    const handleAvatar = (e) => {
+        setAvatar({
+        file: e.target.files[0],
+        url: URL.createObjectURL(e.target.files[0]),
+        });
+    };
 
     const handleRegister = async (e) => {
-        e.preventDefault()
+        e.preventDefault();
+        setLoading(true);
         const formData = new FormData(e.target);
 
-        const {username,email,password} = Object.fromEntries(formData);
+        const { username, email, password } = Object.fromEntries(formData);
 
         try {
-            const res = await createUserWithEmailAndPassword(auth,email,password);
+        const res = await createUserWithEmailAndPassword(auth, email, password);
 
-            const imgUrl = await upload(avatar.file);
-            await setDoc(doc(db, "users", res.user.uid), {
-                username,
-                email,
-                avatar: imgUrl,
-                id: res.user.uid,
-                blocked: [],
-            });
+        const imgUrl = await upload(avatar.file);
+        await setDoc(doc(db, "users", res.user.uid), {
+            username,
+            email,
+            avatar: imgUrl,
+            id: res.user.uid,
+            blocked: [],
+        });
 
-            await setDoc(doc(db, "userchats", res.user.uid), {
-                chats: [],
-            })
+        await setDoc(doc(db, "userchats", res.user.uid), {
+            chats: [],
+        });
 
-            toast.success("Account created! You can login now!")
+        toast.success("Account created! You can login now!");
         } catch (error) {
-            console.log(error);
-            toast.error(error.message)
+        console.log(error);
+        toast.error(error.message);
+        } finally {
+        setLoading(false);
         }
-    }
+    };
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        const formData = new FormData(e.target);
+        const { email, password } = Object.fromEntries(formData);
+
+        try {
+        await signInWithEmailAndPassword(auth, email, password);
+        toast.success("Logged In!");
+        } catch (error) {
+        console.log(error);
+        toast.error(error.message);
+        } finally {
+        setLoading(false);
+        }
+    };
+
     return (
         <div className="h-full w-full flex justify-around items-center">
         {/* login */}
         <div className="w-80">
             <form onSubmit={handleLogin} className="flex flex-col gap-4">
             <h1 className="text-2xl">Welcome back - Buddy!</h1>
-            <input className="bg-white/15 outline-none hover:border border-white/10 transition-all ease-in h-10 w-full px-2 rounded-xl" type="Email" placeholder="Email" name="Email" />
-            <input className="bg-white/15 outline-none hover:border border-white/10 transition-all ease-in h-10 w-full px-2 rounded-xl" type="Password" placeholder="Password" name="Password" />
-            <button className="bg-blue-500 py-2 px-4 rounded-xl hover:opacity-80 transition-all ease-in">Log In</button>
+            <input
+                className="bg-white/15 outline-none hover:border border-white/10 transition-all ease-in h-10 w-full px-2 rounded-xl"
+                type="email"
+                placeholder="Email"
+                name="email"
+            />
+            <input
+                className="bg-white/15 outline-none hover:border border-white/10 transition-all ease-in h-10 w-full px-2 rounded-xl"
+                type="password"
+                placeholder="Password"
+                name="password"
+            />
+            <button
+                disabled={loading}
+                className="bg-blue-500 py-2 px-4 rounded-xl hover:opacity-80 transition-all ease-in disabled:cursor-wait disabled:opacity-80"
+            >
+                {loading ? "Loading" : "Sign In"}
+            </button>
             </form>
         </div>
         {/* seperator */}
@@ -69,14 +107,48 @@ const Login = () => {
         <div className="w-80">
             <form className="flex flex-col gap-4" onSubmit={handleRegister}>
             <h1 className="text-2xl">Create an Account</h1>
-            <label className="flex items-center gap-4 underline cursor-pointer" htmlFor="file">
-                <img className="h-14 rounded-md w-14" src={avatar.url || "List Icons/user-image-with-black-background.png" } />
-                Upload an image</label>
-            <input type="file" id="file" className="hidden" onChange={handleAvatar} />
-            <input className="bg-white/15 outline-none hover:border border-white/10 transition-all ease-in h-10 w-full px-2 rounded-xl" type="text" placeholder="Username" name="username" />
-            <input className="bg-white/15 outline-none hover:border border-white/10 transition-all ease-in h-10 w-full px-2 rounded-xl" type="email" placeholder="Email" name="email" />
-            <input className="bg-white/15 outline-none hover:border border-white/10 transition-all ease-in h-10 w-full px-2 rounded-xl" type="password" placeholder="Password" name="password" />
-            <button className="bg-blue-500 py-2 px-4 rounded-xl hover:opacity-80 transition-all ease-in">Sign In</button>
+            <label
+                className="flex items-center gap-4 underline cursor-pointer"
+                htmlFor="file"
+            >
+                <img
+                className="h-14 rounded-md w-14"
+                src={
+                    avatar.url || "List Icons/user-image-with-black-background.png"
+                }
+                />
+                Upload an image
+            </label>
+            <input
+                type="file"
+                id="file"
+                className="hidden"
+                onChange={handleAvatar}
+            />
+            <input
+                className="bg-white/15 outline-none hover:border border-white/10 transition-all ease-in h-10 w-full px-2 rounded-xl"
+                type="text"
+                placeholder="Username"
+                name="username"
+            />
+            <input
+                className="bg-white/15 outline-none hover:border border-white/10 transition-all ease-in h-10 w-full px-2 rounded-xl"
+                type="email"
+                placeholder="Email"
+                name="email"
+            />
+            <input
+                className="bg-white/15 outline-none hover:border border-white/10 transition-all ease-in h-10 w-full px-2 rounded-xl"
+                type="password"
+                placeholder="Password"
+                name="password"
+            />
+            <button
+                disabled={loading}
+                className="bg-blue-500 py-2 px-4 rounded-xl hover:opacity-80 transition-all ease-in disabled:cursor-wait disabled:opacity-80"
+            >
+                {loading ? "Loading" : "Sign Up"}
+            </button>
             </form>
         </div>
         </div>
